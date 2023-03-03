d7d110e29b8d6e37f6c6263979c95e18925f022b
MKempler <maxkempler@gmail.com>
2023-03-03 14:28:30 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(1337, function() {
-  console.log('Server listening on port 1337.');
+var http = require('http');
+http.createServer(function (request, response) {
+response.writeHead(200, {
+'Content-Type': 'text/plain'
 });
+response.end('Hello World\n');
+}).listen(1337);
+console.log('Server running on port 1337!');
